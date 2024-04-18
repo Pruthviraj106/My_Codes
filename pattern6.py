7feffa4d38532dbe22d8b46daf8e698db848fe10
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-18 09:34:07 +0530

    Create pattern6.py

---
+++ pattern6.py
@@ -0,0 +1,16 @@
+#PATTERN OF FORM
+# *******
+# ******
+# *****
+# ****
+# ***
+# **
+# *
+
+
+#pattern5
+n= int(input())
+for r in range(n,0,-1):
+    for s in range(r):
+        print("*",end="")
+    print()
