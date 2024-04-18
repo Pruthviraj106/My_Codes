dd2c9af9e03da2d316af3c7ff4907debd5116ff0
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-18 09:36:35 +0530

    Update pattern6.py

--- pattern6.py
+++ pattern6.py
@@ -1,16 +1,21 @@
 #PATTERN OF FORM
+#       *
+#      **
+#     ***
+#    ****
+#   *****
+#  ******
 # *******
-# ******
-# *****
-# ****
-# ***
-# **
-# *
 
-
-#pattern5
-n= int(input())
-for r in range(n,0,-1):
-    for s in range(r):
-        print("*",end="")
+#pattern 6  
+n=int(input ())
+spaces=n-1
+stars = 1
+for i in range(n):
+    for j in range (spaces):
+        print(" ", end = "")
+    for j in range (stars):
+        print("*", end="")
     print()
+    spaces = spaces - 1
+    stars=stars+1
