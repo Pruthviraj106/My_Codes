fa216bdab7030712c7bbc5b83933304f43595376
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-21 19:40:32 +0530

    Create patttern13.py

---
+++ patttern13.py
@@ -0,0 +1,15 @@
+# pattern of the form
+# 10
+# 20 30
+# 40 50 60
+# 70 80 90 100
+# 110 120 130 140 150
+
+#pattern 13
+n=int(input())
+val=10
+for i in range(n):
+  for j in range(i+1):
+    print(val,end=" ")
+    val=val+10
+  print()
