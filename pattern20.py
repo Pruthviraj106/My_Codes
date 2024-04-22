307f021b2dcb2c227d56a5ec16f0b86f7f52f343
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-22 20:56:38 +0530

    Create pattern20.py

---
+++ pattern20.py
@@ -0,0 +1,38 @@
+# pattern of form
+
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+
+
+#pattern 20
+n = int(input())
+
+spaces = 0
+stars = 2 * n  - 1
+for i in range(n - 1, 0, -1):
+  for j in range(spaces):
+    print(" ", end=" ")
+  for j in range(stars):
+    print("*", end=" ")
+  print()
+  spaces += 1
+  stars -= 2
+
+spaces = n-1
+stars = 1
+for i in range(n):
+  for j in range(spaces):
+    print(" ", end=" ")
+  for j in range(stars):
+    print("*", end=" ")
+  print()
+  spaces -= 1
+  stars += 2
