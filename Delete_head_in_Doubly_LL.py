1365989a630e066365e761ee38a36fa2ec9c65a5
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-29 21:15:23 +0530

    Create Delete_head_in_Doubly_LL.py

---
+++ Delete_head_in_Doubly_LL.py
@@ -0,0 +1,80 @@
+# Delete head node in Doubly-linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+def printLeftToRightManner(head):
+    curr = head 
+    while curr != None:
+        print(curr.data, end = " ")
+        curr = curr.next 
+    print()
+
+def printRightToLeftManner(tail):
+    tail = head 
+    while tail.next != None:
+        tail = tail.next 
+ 
+    curr = tail
+    while curr != None:
+        print(curr.data, end = " ")
+        curr = curr.prev 
+    print()
+    
+def addNodeAtTailOfLinkedList(head, val):
+    newBlock = Node(val)
+ 
+    if head == None:
+        return newBlock
+ 
+    tail = head 
+    while tail.next != None:
+        tail = tail.next 
+    tail.next = newBlock 
+    newBlock.prev = tail 
+    return head
+    
+def addNodeAtHeadOfLinkedList(head, val):
+    newBlock = Node(val)
+    if head == None:
+        return newBlock
+   
+    newBlock.next = head 
+    head.prev = newBlock 
+    return newBlock
+
+def deleteHeadNodeInDoublyLinkedList(head):
+    if head == None or head.next == None:
+        return None 
+    secondNode = head.next 
+    head.next = None 
+    secondNode.prev = None 
+    return secondNode 
+ 
+
+n = int(input())
+values = list(map(int, input().split()))
+
+head = None
+for value in values:
+    head = addNodeAtTailOfLinkedList(head, value)
+
+    
+printLeftToRightManner(head)
+printRightToLeftManner(head)
+
+
+
+head = deleteHeadNodeInDoublyLinkedList(head)
+
+printLeftToRightManner(head)
+
+
+
+tail = head 
+while tail.next != None:
+    tail = tail.next 
+printRightToLeftManner(tail)
