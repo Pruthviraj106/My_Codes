de4afc5420233f411b6979696deba30eee0f8263
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-29 20:43:26 +0530

    Create Insert_at_tail _of_LL.py

---
+++ Insert_at_tail _of_LL.py
@@ -0,0 +1,20 @@
+# Insert a Node at the Tail of a Linked List
+# FUNCTION
+class SinglyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def insertNodeAtTail(head, data):
+    new_node = SinglyLinkedListNode(data)
+
+    if head is None:
+        return new_node
+
+    current = head
+    while current.next is not None:
+        current = current.next
+
+    current.next = new_node
+
+    return head
