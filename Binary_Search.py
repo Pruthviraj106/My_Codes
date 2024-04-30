8e39081a6bcfce444cf771adf55134a3ba784bec
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-30 11:33:31 +0530

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,24 @@
+# A program to find whether a given target element is present within the list of elements or not
+target=int(input())
+n = int(input().strip())
+num = list(map(int, input().split())) 
+nums = sorted(num)
+
+left = 0
+right = len(nums) - 1
+found = False
+
+while left <= right:
+    mid = (left + right) // 2
+    if nums[mid] == target:
+        found = True
+        break
+    elif nums[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+
+if found:
+    print("Target is present")
+else:
+    print("Target is not present")
