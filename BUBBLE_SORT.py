d22d6fb58ff75842abdf446633218c40f00c73eb
Pruthviraj106 <159701685+Pruthviraj106@users.noreply.github.com>
2024-04-30 11:34:43 +0530

    Create BUBBLE_SORT.py

---
+++ BUBBLE_SORT.py
@@ -0,0 +1,18 @@
+# Bubble-sort Algorithm to sort the given list of numbers
+def Bubblesort(Arr):
+    n= len(Arr)
+
+    for fixtheindex in range(n-1,0,-1):
+        for i in range(fixtheindex) :
+            if Arr[i]>Arr[i+1] :
+                temp=Arr[i]
+                Arr[i]=Arr[i+1]
+                Arr[i+1] = temp
+
+n = int(input().strip())
+Arr = list(map(int, input().split()))
+
+    
+Bubblesort(Arr)
+for i in range(n):
+    print(Arr[i],end=" ")
